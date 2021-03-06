79b3cb495bc957c2da58f61e628e9ffbe3e2b40f
EltrutLikes <10eltrut1@gmail.com>
2021-03-06 15:19:28 -0500

    vertical slab revert recipe

--- src/main/java/co/eltrut/differentiate/core/datagen/helper/CraftingHelper.java
+++ src/main/java/co/eltrut/differentiate/core/datagen/helper/CraftingHelper.java
@@ -5,6 +5,7 @@ import java.util.function.Consumer;
 import org.apache.commons.lang3.ArrayUtils;
 
 import co.eltrut.differentiate.core.datagen.builder.DifferShapedRecipeBuilder;
+import co.eltrut.differentiate.core.datagen.builder.DifferShapelessRecipeBuilder;
 import net.minecraft.block.Block;
 import net.minecraft.data.IFinishedRecipe;
 
@@ -55,7 +56,14 @@ public class CraftingHelper {
     		.build(consumer);
 	}
 	
-	// TODO: vertical slab revert recipe
+	public static void createVerticalSlabRevert(Consumer<IFinishedRecipe> consumer, Block slab, Block verticalSlab, String[] mods, String[] conditions, String[] flags) {
+		DifferShapelessRecipeBuilder.shapelessRecipe(slab)
+			.addIngredient(verticalSlab.asItem())
+			.addModCompat(mods)
+			.addConditions(conditions)
+			.addFlags(ArrayUtils.add(flags, "vertical_slabs"))
+			.build(consumer);
+	}
 	
 	
 }
