5d30ce0b9cc7035442df3aadf975f866dc397fa8
EltrutLikes <10eltrut1@gmail.com>
2022-02-19 13:26:22 -0500

    hardcode vanilla woods so redoing lepton isn't a pain

--- src/main/java/co/eltrut/differentiate/core/util/BlockUtil.java
+++ src/main/java/co/eltrut/differentiate/core/util/BlockUtil.java
@@ -6,7 +6,6 @@ import net.minecraft.core.dispenser.DispenseItemBehavior;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DispenserBlock;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.material.Material;
@@ -56,6 +55,19 @@ public class BlockUtil {
 		public static final Block.Properties JASPER = Block.Properties.of(Material.STONE, MaterialColor.TERRACOTTA_RED).requiresCorrectToolForDrops().strength(1.5F, 6.0F);
 		public static final Block.Properties SLATE = Block.Properties.of(Material.STONE, MaterialColor.ICE).requiresCorrectToolForDrops().strength(1.5F, 6.0F);
 		public static final Block.Properties MYALITE = Block.Properties.of(Material.STONE, MaterialColor.COLOR_PURPLE).requiresCorrectToolForDrops().strength(1.5F, 6.0F);
+
+	}
+
+	public static class WoodProperties {
+
+		public static final MaterialColor ACACIA = MaterialColor.COLOR_GRAY;
+		public static final MaterialColor BIRCH = MaterialColor.SAND;
+		public static final MaterialColor DARK_OAK = MaterialColor.COLOR_BROWN;
+		public static final MaterialColor JUNGLE = MaterialColor.DIRT;
+		public static final MaterialColor OAK = MaterialColor.WOOD;
+		public static final MaterialColor SPRUCE = MaterialColor.PODZOL;
+		public static final MaterialColor CRIMSON = MaterialColor.CRIMSON_HYPHAE;
+		public static final MaterialColor WARPED = MaterialColor.WARPED_HYPHAE;
 
 	}
 	
