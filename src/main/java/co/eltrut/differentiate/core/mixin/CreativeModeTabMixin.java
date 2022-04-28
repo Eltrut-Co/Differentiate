8f1fcb52af7a53176330d0fc1ef57c8a74233f17
EltrutLikes <10eltrut1@gmail.com>
2022-04-28 09:14:57 -0400

    only one tab now

--- src/main/java/co/eltrut/differentiate/core/mixin/CreativeModeTabMixin.java
+++ src/main/java/co/eltrut/differentiate/core/mixin/CreativeModeTabMixin.java
@@ -3,7 +3,9 @@ package co.eltrut.differentiate.core.mixin;
 import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,12 +15,15 @@ import java.util.Comparator;
 @Mixin(CreativeModeTab.class)
 public class CreativeModeTabMixin {
 
-    @Inject(method = "fillItemList(Lnet/minecraft/core/NonNullList;)V", at = @At("TAIL"), cancellable = true)
+    @Shadow @Final
+    private String langId;
+
+    @Inject(method = "fillItemList(Lnet/minecraft/core/NonNullList;)V", at = @At("TAIL"), cancellable = false)
     private void fillItemList(NonNullList<ItemStack> pItems, CallbackInfo ci) {
-        Comparator<ItemStack> comparator = Comparator.comparing(s -> {
-            return s.getItem().getRegistryName().getPath();
-        });
-        pItems.sort(comparator);
+        if (this.langId.equals("buildingBlocks")) {
+            Comparator<ItemStack> comparator = Comparator.comparing(s -> s.getItem().getRegistryName().getPath());
+            pItems.sort(comparator);
+        }
     }
 
 }
