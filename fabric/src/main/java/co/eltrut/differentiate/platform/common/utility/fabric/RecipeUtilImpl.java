3c3a1e650ca9070b00e1171cc6af85fca1d12ac9
EltrutLikes <10eltrut1@gmail.com>
2022-12-30 16:04:44 -0500

    update loom

--- fabric/src/main/java/co/eltrut/differentiate/platform/common/utility/fabric/RecipeUtilImpl.java
+++ fabric/src/main/java/co/eltrut/differentiate/platform/common/utility/fabric/RecipeUtilImpl.java
@@ -1,16 +1,8 @@
 package co.eltrut.differentiate.platform.common.utility.fabric;
 
-import co.eltrut.differentiate.core.datagen.builder.CuttingRecipeBuilder;
-import co.eltrut.differentiate.core.datagen.builder.DifferCookingRecipeBuilder;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.ItemLike;
 
 public class RecipeUtilImpl {
-    public static DifferCookingRecipeBuilder bakingRecipe(Ingredient ingredientIn, ItemLike resultIn, float experienceIn, int cookingTimeIn) {
-        return null;
-    }
 
-    public static CuttingRecipeBuilder sawingRecipe(Ingredient ingredientIn, ItemLike resultIn, int countIn) {
-        return null;
-    }
 }
